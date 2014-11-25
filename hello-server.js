ba4d6147d879ee515849d7e78d9ba8b87222b811
iGuest <agregers@hotmail.com>
2014-11-25 12:05:30 -0800

    finished tuesday lecture

--- hello-server.js
+++ hello-server.js
@@ -6,3 +6,27 @@
 
 'use strict';
 
+var http = require('http');
+
+//var server = http.createServer(function(req, res) {
+//    res.writeHead(200, {'Content-Type': 'text/plain'});
+//    res.end('Hello World! from Alexis!\n');
+
+//});
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+    res.send('Hello World from Alexis!');
+});
+
+app.get('/foo', function(req, res) {
+    res.send('You requested Foo!');
+
+});
+
+app.listen(8080, function(){
+    console.log('Server listening on http://localhost:8080');
+
+});
